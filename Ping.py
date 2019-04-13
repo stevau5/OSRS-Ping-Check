5571f536e5d6883a90feb42a2ac95d3f5353c4cf
Stevau5 <stevenanthonyzanga@gmail.com>
2019-04-13 13:54:45 -0400

    added logistics within worlds and their pings associated with them.

--- Ping.py
+++ Ping.py
@@ -10,18 +10,24 @@ try:
 except: 
     print("no ouput file")
 
+f = open("output.txt", "x")
 
-for x in range(23):
-    print("Current Pinged World: " + str(x + 300))
-    os.system("ping -c 1  -t 1 oldschool"+ str(x) +".runescape.com >> output.txt")
+for x in range(1, 32):
+    os.system("ping -c 1 oldschool"+ str(x) +".runescape.com >> output.txt")
 
-f = open("output.txt")
+count = 301
+
+f = open("output.txt", "r")
 
 for line in f:
     if line.find("time") != -1:
-        print("true   ", line)
-        
+        splitLine = line.split()
+        pingIndex = splitLine[6]
+        ping = pingIndex[5:]
 
+        print("World " + str(count) + " current ping ---> ", ping)
+        count += 1
+        
 
 endtime = time.time()
 
